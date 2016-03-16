8fa0f9e67244d5161fdd5ad73d450ca744c84338
Anthony Neves <anneves728@gmail.com>
2016-03-16 19:59:03 -0400

    blah
    blah

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function my_first_js_function() {
+    var taco = 1;
+    var t = "srt";
+    var k = [1,2,3];
+
+    if (k==12) {
+        return 42
+    }
+
+    for (var i = 0; i < 10; i++){
+
+    }
+
+    return 42;
+
+}
+
+function kosbie_face_click_handler() {
+    alert("Fuck Off!");
+}
+
+function on_page_ready() {
+    $(".Jellyfish").click(kosbie_face_click_handler)
+}
+
+$(document).ready(on_page_ready)
+
+
